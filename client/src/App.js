1fdfa39db1adad9bdf29ca953a3f57e35165ce8a
sriharsha <sriharshadabbiru04@gmail.com>
2025-06-03 17:27:41 +0530

    connect React client to backend using Socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-export default function App() {
+import { socket } from "./socket";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected to server :", socket.id);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
-    <div className="text-3xl font-bold text-blue-600 underline">
-      Tailwind is working!
+    <div className="flex h-screen justify-center items-centre text-xl">
+      Socket.IO is connected!
     </div>
   );
 }
+
+export default App;
